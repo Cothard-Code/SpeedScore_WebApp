b4365b5e5bb24929993e1159e6809144eadffd3b
acothard <aa.cothard@gmail.com>
2022-01-17 00:01:35 -0800

    Ready for heroku16

--- passport/googleStrategy.js
+++ passport/googleStrategy.js
@@ -3,7 +3,41 @@
 //The following code sets up the app with OAuth authentication using
 //the 'google' strategy in passport.js.
 //////////////////////////////////////////////////////////////////////////
-import { GoogleLogin } from "react-google-login";
+import GoogleStrategy from 'passport-google-oauth2'; 
+import User from '../models/User.js';
+
+const googleStrategy = new GoogleStrategy.Strategy ({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: process.env.DEPLOY_URL + "auth/google/callback"
+  },
+  async (accessToken, refreshToken, profile, done) => {
+    console.log("User authenticated through Google. In passport callback.");
+    //Our convention is to build userId from displayName and provider
+    const userId = `${profile.username}@${profile.provider}`;
+    //See if document with this unique userId exists in database 
+    let currentUser = await User.findOne({"accountData.id": userId});
+    if (!currentUser) { //Add this user to the database
+        currentUser = await new User({
+          accountData: {id: userId},
+          identityData: {displayName: profile.displayName,
+                         profilePic: profile.photos[0].value},
+          speedgolfData: {bio: "",
+                          homeCourse: "",
+                          personalBest: {},
+                          clubs: {},
+                          clubComments: ""}
+      }).save();
+    }
+    return done(null,currentUser);
+    }
+);
+
+export default googleStrategy;
+
+
+
+/* import { GoogleLogin } from "react-google-login";
 import User from '../models/User.js';
 
 // Render GoogleLogin component
@@ -36,4 +70,4 @@ const googleStrategy = new GoogleLogin({
   }
 });
 
-export default googleStrategy;
+export default googleStrategy; */
